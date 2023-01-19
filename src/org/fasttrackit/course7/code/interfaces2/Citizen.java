68679edb049b52757dec7ef26e4877ca0fc1118d
raduandreipopescu <popescu.a.radu@gmail.com>
2023-01-19 17:51:53 +0200

    Course 7 code

--- src/org/fasttrackit/course7/code/interfaces2/Citizen.java
+++ src/org/fasttrackit/course7/code/interfaces2/Citizen.java
@@ -2,7 +2,10 @@ package org.fasttrackit.course7.code.interfaces2;
 
 public interface Citizen extends Person {
     String getLanguage();
+
     long getIdentifier();
+
     String getCountry();
+
     void setCountry(String country);
 }
--- src/org/fasttrackit/course7/code/interfaces2/Employee.java
+++ src/org/fasttrackit/course7/code/interfaces2/Employee.java
@@ -2,5 +2,6 @@ package org.fasttrackit.course7.code.interfaces2;
 
 public interface Employee extends Person {
     String getCompany();
+
     int getSalary();
 }
